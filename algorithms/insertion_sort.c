88b7ad7f8a0ea4401ac528a017dff6577afec29f
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-10-19 16:27:58 -0300

    feat: implementar o isertion sort

---
+++ algorithms/insertion_sort.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int *insertion_sort(int *arr, signed int size)
+{
+  for (int i = 1; i < size; i++)
+  {
+    int current_value = arr[i];
+    int previous_index = i - 1;
+
+    while (arr[previous_index] > current_value)
+    {
+      arr[previous_index + 1] = arr[previous_index];
+      previous_index = previous_index - 1;
+    }
+
+    arr[previous_index + 1] = current_value;
+  }
+
+  return arr;
+}
+
---
+++ algorithms/insertion_sort.h
@@ -0,0 +1,1 @@
+int *insertion_sort(int *arr, signed int size);
