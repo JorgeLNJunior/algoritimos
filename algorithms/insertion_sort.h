57e1c7613dfd06e7580b3a2d8c584c45d6a109b5
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-11-03 15:34:57 -0300

    fix: o header deve terminar com "_H"

--- algorithms/insertion_sort.h
+++ algorithms/insertion_sort.h
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 
-#if !defined(INSERTION_SORT)
-#define INSERTION_SORT
+#if !defined(INSERTION_SORT_H)
+#define INSERTION_SORT_H
 int *insertion_sort(int *arr, size_t const size);
 #endif
