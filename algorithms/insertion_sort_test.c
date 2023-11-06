40d710b05f2e66ffb53396748383c916a5c51dd3
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-11-06 14:59:29 -0300

    test: usar a biblioteca unit nos testes

--- algorithms/insertion_sort_test.c
+++ algorithms/insertion_sort_test.c
@@ -3,39 +3,35 @@
 #include <stdlib.h>
 
 #include "insertion_sort.h"
+#include "../libs/unity/unity.h"
 
-bool run_test()
+void test_sort()
 {
-  size_t const ARR_LEN = 10;
+  size_t const ARR_LEN = 50;
   int arr[ARR_LEN];
 
-  for (size_t index = 0; index < ARR_LEN; index++)
+  for (int index = 0; index < ARR_LEN; index++)
   {
     arr[index] = rand();
   }
 
   int const *sorted_arr = insertion_sort(arr, ARR_LEN);
 
-  int previous_value = 0;
-  for (size_t index = 0; index < ARR_LEN; index++)
+  int previous_value = arr[0];
+  for (int index = 1; index < ARR_LEN; index++)
   {
     int const current_value = sorted_arr[index];
-    if(previous_value > current_value) {
-      printf("[insertion_sort] the value '%d' is greater than '%d'", previous_value, current_value);
-      return false;
-    }
+    TEST_ASSERT_GREATER_OR_EQUAL_INT(previous_value, current_value);
     previous_value = current_value;
   }
-
-  return true;
 }
+
+void setUp() {}
+void tearDown() {}
 
 int main()
 {
-  bool const result = run_test();
-  if (result == true) {
-    printf("[insertion_sort] all tests have been executed with success");
-    return 0;
-  }
-  return 1;
+  UNITY_BEGIN();
+  RUN_TEST(test_sort);
+  return UNITY_END();
 }
--- run_tests.sh
+++ run_tests.sh
@@ -1,2 +1,2 @@
-gcc algorithms/insertion_sort_test.c algorithms/insertion_sort.c -o bin/insertion_sort_test.out
+gcc algorithms/insertion_sort_test.c algorithms/insertion_sort.c libs/unity/unity.c -o bin/insertion_sort_test.out
 ./bin/insertion_sort_test.out
