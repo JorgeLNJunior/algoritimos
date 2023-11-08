f4e26151cc579170950936466b5c396fd866e515
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-11-08 16:43:46 -0300

    chore(test): comentar os comandos dos testes

--- run_tests.sh
+++ run_tests.sh
@@ -1,5 +1,7 @@
+# insertion sort
 gcc algorithms/insertion_sort_test.c algorithms/insertion_sort.c libs/unity/unity.c -o bin/insertion_sort_test
 ./bin/insertion_sort_test
 
+# array
 gcc data_structures/array_test.c data_structures/array.c libs/unity/unity.c -o bin/array_test
 ./bin/array_test
