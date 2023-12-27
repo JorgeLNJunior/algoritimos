dd1ee6d1421ed3dc4b4b9a12aac6f1bf415dfcf6
JorgeLNJunior <jorgelnjunior@gmail.com>
2023-12-27 11:23:28 -0300

    feat(table): adicionar suporte a size e capacity

--- data_structures/hash_table.c
+++ data_structures/hash_table.c
@@ -2,27 +2,29 @@
 #include <stdio.h>
 
 char *dummy_key = "<dummy>";
-unsigned int const DEFAULT_TABLE_SIZE = 100;
+unsigned int const DEFAULT_TABLE_CAPACITY = 100;
+unsigned int const GROWTH_FACTOR = 2;
 
-HashTable *hash_table_create(const size_t size)
+HashTable *hash_table_create(const size_t capacity)
 {
   HashTable *table = malloc(sizeof(HashTable));
   if(table == NULL) exit(1);
 
-  table->data = malloc(sizeof(HashTableItem*) * size);
+  table->data = malloc(sizeof(HashTableItem*) * capacity);
 
-  for(size_t index = 0; index < size; ++index)
+  for(size_t index = 0; index < capacity; ++index)
   {
     table->data[index] = NULL;
   }
 
-  table->size = size;
+  table->capacity = capacity;
+  table->size = 0;
   return table;
 }
 
 void hash_table_free(HashTable *table)
 {
-  for(size_t index = 0; index < table->size; ++index)
+  for(size_t index = 0; index < table->capacity; ++index)
   {
     free(table->data[index]);
   }
@@ -30,6 +32,7 @@ void hash_table_free(HashTable *table)
 }
 
 int hash(const char *key, size_t table_size)
+int hash(const char *key, size_t table_capacity)
 {
   unsigned long hash = 2166136261;
   unsigned long const fnv_prime = 16777619;
@@ -40,12 +43,12 @@ int hash(const char *key, size_t table_size)
     hash = (hash * fnv_prime);
   }
 
-  return hash % table_size;
+  return hash % table_capacity;
 }
 
 void hash_table_add(HashTable *table, char *key, char *value)
 {
-  unsigned int const position = hash(key, table->size);
+  unsigned int const position = hash(key, table->capacity);
 
   if(table->data[position] == NULL)
   {
@@ -53,6 +56,7 @@ void hash_table_add(HashTable *table, char *key, char *value)
     item->key = key;
     item->value = value;
     table->data[position] = item;
+    table->size = table->size + 1;
     return;
   }
 
@@ -61,7 +65,7 @@ void hash_table_add(HashTable *table, char *key, char *value)
     return;
   }
 
-  for(size_t index = position + 1; index < table->size; ++index)
+  for(size_t index = position + 1; index < table->capacity; ++index)
   {
     if(table->data[index] == NULL) {
       HashTableItem *item = malloc(sizeof(HashTableItem));
@@ -75,12 +79,12 @@ void hash_table_add(HashTable *table, char *key, char *value)
 
 bool hash_table_key_exists(const HashTable *table, const char *key)
 {
-  unsigned int const position = hash(key, table->size);
+  unsigned int const position = hash(key, table->capacity);
 
   if(table->data[position] == NULL) return false;
   if(strcmp(table->data[position]->key, key) == 0) return true;
 
-  for(int index = position + 1; index < table->size; ++index)
+  for(int index = position + 1; index < table->capacity; ++index)
   {
     if(table->data[index] == NULL) return false;
     if(strcmp(table->data[index]->key, key) == 0) return true;
@@ -91,12 +95,12 @@ bool hash_table_key_exists(const HashTable *table, const char *key)
 
 char *hash_table_get(const HashTable *table, const char *key)
 {
-  unsigned int const position = hash(key, table->size);
+  unsigned int const position = hash(key, table->capacity);
 
   if(table->data[position] == NULL) return NULL;
   if(strcmp(table->data[position]->key, key) == 0) return table->data[position]->value;
 
-  for(int index = position + 1; index < table->size; ++index)
+  for(int index = position + 1; index < table->capacity; ++index)
   {
     if(table->data[index] == NULL) return NULL;
     if(strcmp(table->data[index]->key, key) == 0) return table->data[index]->value;
@@ -105,22 +109,24 @@ char *hash_table_get(const HashTable *table, const char *key)
   return NULL;
 }
 
-void hash_table_remove(const HashTable *table, const char *key)
+void hash_table_remove(HashTable *table, const char *key)
 {
-  unsigned int const position = hash(key, table->size);
+  unsigned int const position = hash(key, table->capacity);
 
   if(table->data[position] == NULL) return;
   if(strcmp(table->data[position]->key, key) == 0)
   {
     table->data[position]->key = dummy_key;
+    table->size = table->size - 1;
     return;
   }
 
-  for(int index = position + 1; index < table->size; ++index)
+  for(int index = position + 1; index < table->capacity; ++index)
   {
     if(table->data[index] == NULL) return;
     if(strcmp(table->data[index]->key, key) == 0) {
       table->data[index]->key = dummy_key;
+      table->size = table->size - 1;
       break;
     }
   }
